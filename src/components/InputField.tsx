6f4e7c388b41ffed510c48b6e5c29d7f608b4b3b
Kiran Boyle <anthony.kiran.boyle@gmail.com>
2021-11-01 13:32:07 +0100

    added register page, inputField component

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,5 +1,24 @@
 import React from "react";
-import {useField} from "formik"
+import { useField } from "formik"
+import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/form-control";
+import { Input } from "@chakra-ui/input";
+import { values } from "lodash";
 
-interface InputFieldProps {}
-export const InputField: React.FC<InputFieldProps>
+type InputFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
+    name: string;
+};
+
+export const InputField: React.FC<InputFieldProps> = (props) => {
+    const [field, { error }] = useField(props);
+
+    return (
+        <FormControl isInvalid={!!error}>
+            <FormLabel htmlFor="username">Username</FormLabel>
+            <Input />
+            {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
+        </FormControl>
+    )
+
+
+
+}
